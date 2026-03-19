d998be81ca26e734c640f9b4056a4fb8b6afad9e
Spencer Niemeyer <spencerniemeyer@gmail.com>
2026-03-19 16:21:46 -0500

    Added QM term structs

--- qm.h
+++ qm.h
@@ -1,6 +1,19 @@
 #ifndef QM_H
 #define QM_H
 
+typedef struct {
+    unsigned int value; // Bit value (e.g. 010)
+    unsigned int mask;  // Dash positions (1 = dash)
+    int used;           // 1 if combined in a certain round
+    int * covers;       // Array of minterms this term covers
+    int cover_count;    // Number of minterms covered (in covers array)
+} Term;
+
+typedef struct {
+    Term * terms;       // Array of terms
+    int count;          // Number of terms (in terms array)
+    int capacity;       // Capacity allocated to terms array
+} TermList;
 
 
 
